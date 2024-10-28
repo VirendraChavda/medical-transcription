d66543c12fe8d215e85f6b85993bdc59387453a5
Virendrasinh Chavda <168742290+VirendraChavda@users.noreply.github.com>
2024-10-28 13:07:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -137,4 +137,4 @@ def handle_toggle_transcription():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port)
